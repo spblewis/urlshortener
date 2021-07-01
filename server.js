94f29a293cff671f2708c403e67d5564c32b416d
Schuyler Lewis <spblewis@gmail.com>
2021-07-01 20:25:16 +0000

    basic function to return json object on post to /api/shorturl.  No database interaction yet

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
@@ -10,6 +11,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -17,10 +19,20 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+//Mongoose stuff can go here
+
+
+
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+
+app.post('/api/shorturl', (req, res) => {
+  res.json({ original_url: req.body.url, short_url: 1});
+});
+
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
